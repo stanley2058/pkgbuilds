bff73eb49a67075def1dba2ec734036df018e030
stanley2058 <stanley2058@yahoo.com.tw>
2022-12-25 16:02:23 +0800

    fix: make sure version is not NULL

--- updates/moar-bin/update.sh
+++ updates/moar-bin/update.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
+CURRENT_VER=$(grep "pkgver=" "$PKG_ROOT/PKGBUILD" | sed -r 's/pkgver=//')
 GITHUB_API="https://api.github.com/repos/walles/moar/releases/latest"
 VERSION=$(curl -sH "Accept: application/vnd.github+json" "$GITHUB_API" | jq -r ".tag_name" | sed -e 's/v//')
 
-curl -sL "https://github.com/walles/moar/archive/refs/tags/v${VERSION}.tar.gz" > "$TMP/moar.tar.gz"
-SHA256SUM=$(sha256sum "$TMP/moar.tar.gz" | awk '{ print $1 }')
+[ -z "$VERSION" ] && exit 1
 
-sed -i "s/pkgver=.*/pkgver=$VERSION/" "$PKG_ROOT/PKGBUILD"
-sed -i "s/sha256sums=('[^']*')/sha256sums=('$SHA256SUM')/" "$PKG_ROOT/PKGBUILD"
+if [ "$CURRENT_VER" != "$VERSION" ]; then
+    echo "TEST?"
+    curl -sL "https://github.com/walles/moar/archive/refs/tags/v${VERSION}.tar.gz" > "$TMP/moar.tar.gz"
+    SHA256SUM=$(sha256sum "$TMP/moar.tar.gz" | awk '{ print $1 }')
 
-pushd "$PKG_ROOT" &>/dev/null || exit 1
-update_srcinfo
-popd &>/dev/null || exit 1
+    sed -i "s/pkgver=.*/pkgver=$VERSION/" "$PKG_ROOT/PKGBUILD"
+    sed -i "s/sha256sums=('[^']*')/sha256sums=('$SHA256SUM')/" "$PKG_ROOT/PKGBUILD"
+
+    pushd "$PKG_ROOT" &>/dev/null || exit 1
+    update_srcinfo
+    popd &>/dev/null || exit 1
+fi
