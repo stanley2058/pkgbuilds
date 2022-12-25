eaa3eba4ceff6be9dffaa89aafe3cdfb094a23de
stanley2058 <stanley2058@yahoo.com.tw>
2022-12-25 16:08:27 +0800

    debug: add debug msg to build script

--- updates/moar-bin/update.sh
+++ updates/moar-bin/update.sh
@@ -2,13 +2,15 @@
 
 CURRENT_VER=$(grep "pkgver=" "$PKG_ROOT/PKGBUILD" | sed -r 's/pkgver=//')
 GITHUB_API="https://api.github.com/repos/walles/moar/releases/latest"
-VERSION=$(curl -sH "Accept: application/vnd.github+json" "$GITHUB_API" | jq -r ".tag_name" | sed -e 's/v//')
+VERSION=$(curl -H "Accept: application/vnd.github+json" "$GITHUB_API" | jq -r ".tag_name" | sed -e 's/v//')
+
+echo "Current version: $CURRENT_VER"
+echo "Upstream version: $VERSION"
 
 [ -z "$VERSION" ] && exit 1
 
 if [ "$CURRENT_VER" != "$VERSION" ]; then
-    echo "TEST?"
-    curl -sL "https://github.com/walles/moar/archive/refs/tags/v${VERSION}.tar.gz" > "$TMP/moar.tar.gz"
+    curl -L "https://github.com/walles/moar/archive/refs/tags/v${VERSION}.tar.gz" > "$TMP/moar.tar.gz"
     SHA256SUM=$(sha256sum "$TMP/moar.tar.gz" | awk '{ print $1 }')
 
     sed -i "s/pkgver=.*/pkgver=$VERSION/" "$PKG_ROOT/PKGBUILD"
